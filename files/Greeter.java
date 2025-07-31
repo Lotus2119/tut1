9aaf314579fcffa73378279ff05a276bae4ce93e
Louie <louisk02@icloud.com>
2025-07-31 19:17:41 +1200

    Fixed bug

--- files/Greeter.java
+++ files/Greeter.java
@@ -5,6 +5,7 @@ public class Greeter {
 
   private Locale locale;
   private String name;
+  private Int num;
 
   public Greeter(String language, String country, String name) {
     locale = new Locale(language, country);
